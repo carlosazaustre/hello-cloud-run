ad2d961f361916b2bb9a49d33d0eb5b9c3f1602d
Carlos Azaustre <cazaustre@gmail.com>
2019-07-02 23:20:21 +0200

    added new key-value

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.get('/', (req, res) => {
   res.json({
     date: today,
     up: `${(Date.now() - dateUp)/1000} seg.`,
+    repo: 'https://github.com/carlosazaustre/hello-cloud-run'
   });
 });
 
