7235ba9b14371989d58a18829773107c63c31686
Carlos Azaustre <cazaustre@gmail.com>
2019-07-02 22:56:56 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const dateUp = Date.now();
+
+app.get('/', (req, res) => {
+  const today = new Date();
+
+  res.json({
+    date: today,
+    up: `${(Date.now() - dateUp)/1000} seg.`,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+  console.log('Press CTRL + C to quit');
+});
