4eee342771b0f8943ef355c4a1d75953bc94c157
Carlos Azaustre <cazaustre@gmail.com>
2019-07-02 23:35:17 +0200

    new version

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.get('/', (req, res) => {
   res.json({
     date: today,
     up: `${(Date.now() - dateUp)/1000} seg.`,
-    repo: 'https://github.com/carlosazaustre/hello-cloud-run'
+    repo: 'https://github.com/carlosazaustre/hello-cloud-run',
+    url: 'https://carlosazaustre.es'
   });
 });
 
